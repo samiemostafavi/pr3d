f9f5604f463118714838e6af82ec59fb8d1cb537
Samie Mostafavi <ssmos@kth.se>
2023-02-22 20:42:29 +0100

    add cond_gaussian_mevm

--- pr3d/de/__init__.py
+++ pr3d/de/__init__.py
@@ -1,5 +1,6 @@
 from .cond_gamma_evm import ConditionalGammaEVM
 from .cond_gamma_mevm import ConditionalGammaMixtureEVM
+from .cond_gaussian_mevm import ConditionalGaussianMixtureEVM
 from .cond_gaussian_mm import ConditionalGaussianMM
 from .gamma_evm import GammaEVM
 from .gaussian_mm import GaussianMM
