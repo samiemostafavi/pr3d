a9537ea04dcbd61705433d5a3457755b46b98a6d
Samie Mostafavi <ssmos@kth.se>
2023-04-29 13:45:19 +0200

    Update __init__.py

--- pr3d/de/__init__.py
+++ pr3d/de/__init__.py
@@ -2,5 +2,6 @@ from .cond_gamma_evm import ConditionalGammaEVM
 from .cond_gamma_mevm import ConditionalGammaMixtureEVM
 from .cond_gaussian_mevm import ConditionalGaussianMixtureEVM
 from .cond_gaussian_mm import ConditionalGaussianMM
+from .gaussian_mevm import GaussianMixtureEVM
 from .gamma_evm import GammaEVM
 from .gaussian_mm import GaussianMM
